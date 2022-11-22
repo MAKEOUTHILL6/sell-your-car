db176bcfc244fa97fc521ffcb4b40810a618bf1a
Martin Atanasov <106104073+MAKEOUTHILL6@users.noreply.github.com>
2022-11-22 12:57:44 +0200

    Carwow youtube added plus sell section

--- components/HeroSection/HeroSection.tsx
+++ components/HeroSection/HeroSection.tsx
@@ -42,28 +42,47 @@ const HeroSection = () => {
           </button>
         </div>
       </div>
-      <div className="w-full h-96 bg-white absolute rounded-full top-80 flex ">
-        <article className="bg-slate-100 w-96 h-52 absolute bottom-2 left-32 rounded-xl">
+      <div className="w-full h-full bg-white absolute rounded-full top-80 flex ">
+        <article className="bg-slate-100 w-96 h-52 absolute top-28 left-32 rounded-xl">
           <div className="flex flex-col text-center items-center">
             <TbFreeRights className="text-5xl mt-11 text-rose-500" />
             <p className="font-bold text-xl mt-2">Free</p>
-            <p className="mt-2">100% free, no fees, just great prices!</p>
+            <p className="mt-2 ">100% free, no fees, just great prices!</p>
           </div>
         </article>
-        <article className="bg-slate-100 w-96 h-52 absolute bottom-2 right-37% rounded-xl">
+        <article className="bg-slate-100 w-96 h-52 absolute top-28 right-37% rounded-xl">
           <div className="flex flex-col text-center items-center">
             <VscWorkspaceTrusted className="text-5xl mt-11 text-rose-500" />
             <p className="font-bold text-xl mt-2">Trusted</p>
-            <p className="mt-2">Over 1000 positive reviews!</p>
+            <p className="mt-2 ">Over 1000 positive reviews!</p>
           </div>
         </article>
-        <article className="bg-slate-100 w-96 h-52 absolute bottom-2 right-32 rounded-xl pl-6">
+        <article className="bg-slate-100 w-96 h-52 absolute top-28 right-32 rounded-xl pl-6">
           <div className="flex flex-col text-center items-center">
             <BiWorld className="text-5xl mt-11 text-rose-500" />
             <p className="font-bold text-xl mt-2">Worldwide</p>
-            <p className="mt-2">Worldwide shipping services!</p>
+            <p className="mt-2 ">Worldwide shipping services!</p>
           </div>
         </article>
+
+        <section className={styles.sellcontainer}>
+          <div className="">
+            <div className="w-80 pl-5 pt-6">
+              <h2 className="text-white font-bold text-3xl mb-4">The fastest way to sell your car through our services</h2>
+              <p className="text-white text-lg  mb-8">Free and easy way to get buyers all over the world bidding on your car</p>
+              <button className="bg-white text-lg font-bold py-3 rounded-xl px-24">Sell my car</button>
+            </div>
+          </div>
+        </section>
+
+        <section className={styles.youtubecontainer}>
+            <video className="" src="https://carwow-uk-3.imgix.net/homepage/video/youtube.mp4" autoPlay loop></video>
+            <div className="absolute w-96 pl-5 pt-6 z-10">
+              <h2 className="font-bold text-3xl text-white mb-4">Site inspired by the most popular car Youtube channel</h2>
+              <p className="text-white text-lg mb-20">With over 70+ million views a month, there is a review for everyone</p>
+              <a className="text-white font-bold underline text-lg " href="https://www.youtube.com/channel/UCUhFaUpnq31m6TNX2VKVSVA" target="_blank">Watch on Youtube</a>
+            </div>
+        </section>
       </div>
     </>
   );
---
+++ components/InfoSection/InfoSection.tsx
@@ -0,0 +1,17 @@
+
+const InfoSection = () => {
+
+    return(
+       <section>
+        <div>
+            Sell your car
+        </div>
+
+        <div>
+            Youtube Channel
+        </div>
+       </section>
+    )
+}
+
+export default InfoSection;
